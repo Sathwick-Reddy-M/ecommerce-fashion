b423cc73a222df264813596776d0ca50e1661fc5
Sathwick Reddy Mora <sathwickreddy2002@gmail.com>
2023-10-31 10:07:50 -0500

    use redux thunk for categories async retreival with loading screen

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,11 +4,14 @@ import { useParams } from "react-router-dom";
 import { Fragment } from "react";
 import { CategoryContainer, Title } from "./category.styles";
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { selectCategoriesIsLoading } from "../../store/categories/category.selector";
 
 function Category() {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -18,12 +21,16 @@ function Category() {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 }
